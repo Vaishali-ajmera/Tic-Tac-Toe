af600579dfc2157ccb2d38ffd843ea54db0c06b9
Vaishali <vaishaliajmera11@gmail.com>
2023-03-27 20:45:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const squares = document.querySelectorAll('td');
+const result = document.querySelector('#result');
+const restart = document.querySelector('#restart');
+
+let currentPlayer = 'X';
+let gameActive = true;
+let board = ['', '', '', '', '', '', '', '', ''];
+
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+function handleSquareClick(event) {
+  const square = event.target;
+  const squareIndex = parseInt(square.getAttribute('id'));
+
+  if (board[squareIndex] === '' && gameActive) {
+    board[squareIndex] = currentPlayer;
+    square.textContent = currentPlayer;
+    square.classList.add(currentPlayer);
+    checkResult();
+    togglePlayer();
+  }
+}
+
+function togglePlayer() {
+  currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+}
+
+function checkResult() {
+  let roundWon = false;
+  for (let i = 0; i < winningConditions.length; i++) {
+    const [a, b, c] = winningConditions[i];
+    if (board[a] !== '' && board[a] === board[b] && board[a] === board[c]) {
+      roundWon = true;
+      break;
+    }
+  }
+
+  if (roundWon) {
+    result.textContent = `Player ${currentPlayer} won!`;
+    gameActive = false;
+  } else if (!board.includes('')) {
+    result.textContent = "It's a tie!";
+    gameActive = false;
+  }
+}
+
+function handleRestartClick() {
+  currentPlayer = 'X';
+  gameActive = true;
+  board = ['', '', '', '', '', '', '', '', ''];
+  result.textContent = '';
+  squares.forEach(square => {
+    square.textContent = '';
+    square.classList.remove('X', 'O');
+  });
+}
+
+squares.forEach(square => {
+  square.addEventListener('click', handleSquareClick);
+});
+
+restart.addEventListener('click', handleRestartClick);
